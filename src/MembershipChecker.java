fee839e2c2d74de8f702d96bbf4f5c4107adf1af
Y3LL0W <Iamyellowhead@gmail.com>
2024-10-21 23:39:32 +0200

    final

--- src/MembershipChecker.java
+++ src/MembershipChecker.java
@@ -4,24 +4,56 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class MembershipChecker {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+
+    private boolean found = false;
+    private String currentStatus = null;
+
+    public MembershipChecker(){
+
+        try{
+            Scanner scanner = new Scanner(System.in);
+
+        while(true){
         System.out.println("Enter social security number or name:");
         String input = scanner.nextLine();
 
         checkMembershipStatus(input);
-        scanner.close();
+
+        }
+        }catch(NoSuchElementException e){
+            System.out.println(e.getMessage());
+        }
     }
 
-    public static void checkMembershipStatus(String input) {
+    public MembershipChecker(String input){
+        checkMembershipStatus(input);
+    }
+
+
+    public static void main(String[] args) {
+
+        MembershipChecker checker = new MembershipChecker();
+    }
+
+    public boolean isFound() {
+        return found;
+    }
+
+    public String getCurrentStatus() {
+        return currentStatus;
+    }
+
+    public void checkMembershipStatus(String input) {
         try (BufferedReader br = new BufferedReader(new FileReader("Membership.txt"))) {
             String line;
-            boolean found = false;
+            found = false;
 
             while ((line = br.readLine()) != null) {
                 String[] memberInfo = line.split(",", 2);
@@ -36,31 +68,41 @@ public class MembershipChecker {
                 LocalDate lastPaymentDate = LocalDate.parse(lastPaymentDateLine.trim(), DateTimeFormatter.ofPattern("yyyy-MM-dd"));
                 long daysSinceLastPayment = ChronoUnit.DAYS.between(lastPaymentDate, LocalDate.now());
 
-                if (input.equals(personalNumber) || input.equalsIgnoreCase(name)) {
-                    found = true;
-                    if (daysSinceLastPayment <= 365) {
-                        System.out.println(name + " is a current member");
-                    } else {
-                        System.out.println(name + " is a former member");
-                    }
-                    // Log the member's visit
-                    logMemberVisit(name, personalNumber);
-                    break;
-                }
+                checkStatus(input, name, personalNumber, daysSinceLastPayment);
             }
 
             if (!found) {
                 System.out.println("The person is not found in the system and has never been a member");
+                currentStatus = "not member";
             }
+
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
+    public void checkStatus(String input, String name, String personalNumber, long daysSinceLastPayment) {
+        if (input.equals(personalNumber) || input.equalsIgnoreCase(name)) {
+            found = true;
+            if (daysSinceLastPayment <= 365) {
+                System.out.println(name + " is a current member");
+                currentStatus = "member";
+                logMemberVisit(name, personalNumber);
 
-    public static void logMemberVisit(String name, String personalNumber) {
+            } else {
+                System.out.println(name + " is a former member");
+                currentStatus = "former";
+            }
+
+        }
+    }
+
+
+    public void logMemberVisit(String name, String personalNumber) {
+        LocalDateTime time = LocalDateTime.now();
+
         try (BufferedWriter writer = new BufferedWriter(new FileWriter("GymVisits.txt", true))) {
-            writer.write(name + ": 1");
+            writer.write(name + ": " + personalNumber +": " + time.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")));
             writer.newLine();
         } catch (IOException e) {
             e.printStackTrace();
--- test/MembershipCheckerTest.java
+++ test/MembershipCheckerTest.java
@@ -1,3 +1,66 @@
+import org.junit.jupiter.api.Test;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class MembershipCheckerTest {
+
+@Test
+    public void checkIfUserFound() {
+
+    MembershipChecker checker = new MembershipChecker("Alhambra Aromes");
+
+    assertTrue(checker.isFound());
+
+    checker = new MembershipChecker("7608021234");
+
+    assertTrue(checker.isFound());
+}
+
+@Test
+    public void checkStatus() {
+    MembershipChecker checker = new MembershipChecker("");
+
+    checker.checkStatus("test", "test", "123", 35);
+    assertEquals("member", checker.getCurrentStatus());
+
+    checker.checkStatus("test", "test", "123", 465);
+    assertEquals("former", checker.getCurrentStatus());
+
+    checker = new MembershipChecker("");
+    assertEquals("not member", checker.getCurrentStatus());
+
+}
+
+@Test
+    public void checkPtPrint(){
+    try (BufferedReader br = new BufferedReader(new FileReader("GymVisits.txt"))) {
+
+        MembershipChecker checker = new MembershipChecker("");
+        checker.checkStatus("test", "test", "123", 65);
+
+        LocalDateTime time = LocalDateTime.now();
+        String current;
+        String last = null;
+
+        while ((current = br.readLine()) != null) {
+            last = current;
+        }
+
+        assertEquals("test: 123: " + time.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")), last);
+    }
+    catch (IOException e) {
+        e.printStackTrace();
+    }
+    }
+
+
+
 
 }
